b23f39a04e2ccd795de02a849bd12b36ee8a9e74
Christopher Boyle <cboyle824@gmail.com>
2021-09-02 09:39:19 -0400

    wip

---
+++ config.js
@@ -0,0 +1,5 @@
+// this file is good for pulling stuff out of .env so it's easier to use in the project
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+};
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("yo!!!!");
+require("dotenv").config(); // this allows to inect fake environment variables
+
+const { PORT } = require("./config");
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h1>Web 45 is pretty cool</h1>
+  `);
+});
+
+server.use("*", (req, res) => {
+  res.json({
+    message: "web45 is awesome",
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
